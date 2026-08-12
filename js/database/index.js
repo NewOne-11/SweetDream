dbbd87b9f8ce1e2abf4a30ba417bc955844479c5
NewOne-11 <g4g84vw58n@privaterelay.appleid.com>
2026-08-12 10:40:58 +0800

    Create index.js

---
+++ js/database/index.js
@@ -0,0 +1,18 @@
+/**
+ * SweetDream
+ * Database Module Entry
+ */
+
+export {
+    DatabaseManager
+} from "./databaseManager.js";
+
+export {
+    DatabaseLoader
+} from "./databaseLoader.js";
+
+export {
+    DATABASE_CONFIG,
+    getDatabaseUrl,
+    getDatabaseNames
+} from "./databaseConfig.js";
