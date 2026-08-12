76764d01fbcb15f7bf0c2b007d246ec89d69cf7b
NewOne-11 <g4g84vw58n@privaterelay.appleid.com>
2026-08-12 21:32:36 +0800

    Update creator.js

--- js/creator.js
+++ js/creator.js
@@ -818,7 +818,7 @@ function updateConditions(){
 
 
 
-window.saveCharacter = saveCharacter;
+
 
 
 openDatabase()
@@ -826,4 +826,11 @@ openDatabase()
 
     loadSchema();
 
+});
+
+
+    const saveBtn = document.getElementById("btn-save-character");
+    if (saveBtn) {
+        saveBtn.addEventListener("click", saveCharacter);
+    }
 });
